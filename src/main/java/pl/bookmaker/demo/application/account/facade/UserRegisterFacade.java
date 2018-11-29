d20104992a11de7dc01787cbba0c89f7bb1c2386
cayman25 <33206754+cayman25@users.noreply.github.com>
2018-11-29 20:31:08 +0100

    Delete not finished functionality.

--- src/main/java/pl/bookmaker/demo/application/account/facade/UserRegisterFacade.java
+++
@@ -1,8 +0,0 @@
-package pl.bookmaker.demo.application.account.facade;
-
-import pl.bookmaker.demo.application.account.dto.RegisterUsingCredentials;
-import pl.bookmaker.demo.application.user.dto.UserResponse;
-
-public interface UserRegisterFacade {
-    UserResponse registerUserUsingCredentials(RegisterUsingCredentials request);
-}
