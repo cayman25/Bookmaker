0efc7b3fc6d062c7de461c2ce1d83234772e815e
cayman25 <33206754+cayman25@users.noreply.github.com>
2018-11-29 20:30:53 +0100

    Delete not finished functionality.

--- src/main/java/pl/bookmaker/demo/infrastructure/account/factory/AccountFactory.java
+++
@@ -1,4 +0,0 @@
-package pl.bookmaker.demo.infrastructure.account.factory;
-
-public class AccountFactory {
-}
--- src/main/java/pl/bookmaker/demo/service/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package pl.bookmaker.demo.service.user.service;
-
-import pl.bookmaker.demo.domain.model.user.User;
-
-public interface UserService {
-
-    User getUserById(int userId);
-
-}
